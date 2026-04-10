d7e543716037c3806c633820c423590a723d5d41
Juan Pablo Roldan <juancaicedo1997@gmail.com>
2026-04-10 15:19:34 -0500

    feat: initialize DataTables with export buttons for Fichas, Usuarios, and Sedes tables

--- vistas/js/plantilla.js
+++ vistas/js/plantilla.js
@@ -3,60 +3,20 @@ $(function () {
         {
             "responsive": true, "lengthChange": false, "autoWidth": false,
             "buttons": ["excel", "pdf"]
-            "responsive": true, "lengthChange": false, "autoWidth": false,
-            "buttons": ["excel", "pdf"]
         }).buttons().container().appendTo('#tblApoyos_wrapper .col-md-6:eq(0)');
 
     $("#tblUsuarios").DataTable(
         {
             "responsive": true, "lengthChange": false, "autoWidth": false,
-            "language": {
-                "search": "",
-                "searchPlaceholder": "Buscar Usuario",
-                "info": "Mostrando página _PAGE_ de _PAGES_",
-                "infoEmpty": "No hay registros disponibles",
-                "infoFiltered": "(filtrado de _MAX_ registros totales)",
-                "paginate": {
-                    "first": "Primero",
-                    "last": "Último",
-                    "next": "Siguiente",
-                    "previous": "Anterior"
-                }
-            },
-            "buttons": [
-                {
-                    extend: 'excel',
-                    text: 'EXCEL',
-                    className: 'btn btn-success btn-sm'
-                },
-                {
-                    extend: 'pdf',
-                    text: 'PDF',
-                    className: 'btn btn-success btn-sm'
-                }
-            ]
+            "buttons": ["excel", "pdf"]
         }).buttons().container().appendTo('#tblUsuarios_wrapper .col-md-6:eq(0)');
 
-    $('#example2').DataTable({
-        "paging": true,
-        "lengthChange": false,
-        "searching": false,
-        "ordering": true,
-        "info": true,
-        "autoWidth": false,
-        "responsive": true,
-    }
-    );
-    }
-    );
-});
-
-$(function () {
     $("#tblSedes").DataTable(
         {
             "responsive": true, "lengthChange": false, "autoWidth": false,
             "buttons": ["excel", "pdf"]
         }).buttons().container().appendTo('#tblSedes_wrapper .col-md-6:eq(0)');
+
     $('#example2').DataTable({
         "paging": true,
         "lengthChange": false,
