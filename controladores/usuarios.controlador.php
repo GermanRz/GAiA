3f61040d49c603a4a54eb2aa6090360f469a9389
German Ramirez <germanramirez@outlook.com>
2026-04-24 19:39:20 -0500

    fix: correct method name and improve password validation in user controller

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -1,26 +1,30 @@
 <?php
 
 class ControladorUsuarios{
-    public function ctrIngresoUsuario(){
+
+    public function ctrIngresarUsuario(){
         if (isset($_POST["ingDocumento"])){
             if (
                 preg_match('/^[0-9]+$/', $_POST["ingDocumento"]) &&
-                preg_match('/^[a-z A-Z 0-9]+$/', $_POST["ingPassword"])
+                preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingPassword"])
             ){
                 $documento = $_POST["ingDocumento"];
                 $respuesta = ModeloUsuarios::mdlIngresarUsuario($documento);
-                // var_dump($respuesta["password"]);
 
                 if (is_array($respuesta)){
-                    if ($respuesta["password"] == $_POST["ingPassword"] && $respuesta["documento_id"] == $documento){
+                    if ($respuesta["password"] == $_POST["ingPassword"] && $respuesta["documento_id"]== $documento){
                         $_SESSION["iniciarSesion"] = "ok";
-                        echo "<script> window.location = 'inicio'</script>";
-                    }
-                } else {
-                    echo "<div class='alert alert-danger'>Usuario y/o Contraseña incorrectos</div>";
+                        echo "<script>window.location = 'inicio';</script>";
+                    } else{
+                    // var_dump($respuesta);
+                    echo  "<br><div class='alert alert-danger'>Usuario o contraseña incorrecto</div>";
                     return;
+                    }                      
                 }
-            }
+
+            }    
+            
         }
-    }
-}
+    } //fin del metodo de ingresar usuario
+
+}//fin de la clase ControladorUsuarios
